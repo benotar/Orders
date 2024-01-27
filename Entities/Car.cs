9255fc12189808af592efd60443258702746d188
Ivan <zhmur.ivan@gmail.com>
2024-01-27 21:42:17 +0200

    Add entity - Car

---
+++ Entities/Car.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dotnet_Exam.Entities;
+
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column("brand")]
+    public string? Brand {  get; set; }
+
+    [Column("model")]
+    public string? Model { get; set; }
+
+    [Column("year")]
+    public int Year { get; set; }
+
+    public int ClientId { get; set; }
+    public Client? Client { get; set; }
+}
--- Entities/Client.cs
+++ Entities/Client.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Dotnet_Exam.Entities
+namespace Dotnet_Exam.Entities;
+
+public class Client
 {
-    internal class Client
-    {
-    }
+    [Key]
+    public int Id { get; set; }
+
+    [Column("first_name")]
+    public string? FirstName { get; set; }
+
+    [Column("last_name")]
+    public string? LastName { get; set; }
+
+    public List<Car> Cars { get; set; } = new List<Car>();
+    public List<ClientAddress> ClientAddresses = new List<ClientAddress>();
 }
