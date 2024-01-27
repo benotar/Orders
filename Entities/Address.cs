a9d05b0c4e589925167bdd614d3ea8a69b076225
Ivan <zhmur.ivan@gmail.com>
2024-01-27 21:45:19 +0200

    Add entity - Address

---
+++ Entities/Address.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dotnet_Exam.Entities;
+
+public class Address
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column("street")]
+    public string? Street { get; set; }
+
+    [Column("number")]
+    public int Number { get; set; }
+}
