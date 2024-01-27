a0218af6521d4cc96f2b79d2f08905722009ecd4
Ivan <zhmur.ivan@gmail.com>
2024-01-27 22:21:36 +0200

    Added context class for database and set the configuration in the constructor

---
+++ Context/DotnetExambdContext.cs
@@ -0,0 +1,24 @@
+﻿using Dotnet_Exam.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dotnet_Exam.Context;
+
+public class DotnetExambdContext : DbContext
+{
+    public DbSet<Client> Clients => Set<Client>();
+
+    public DbSet<Car> Cars => Set<Car>();
+
+    public DbSet<Address> Addresss => Set<Address>();
+
+    public DbSet<ClientAddress> ClientAddresses => Set<ClientAddress>();
+    
+    public DbSet<Orders> Orders => Set<Orders>();
+
+    public DotnetExambdContext(DbContextOptions<DbContextOptions> options)
+        :base(options)
+    {
+        Database.EnsureDeleted();
+        Database.EnsureCreated();
+    }
+}
