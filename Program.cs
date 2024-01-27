ea89d90a103ee1b888a4d1576fa5fea0e5296ab0
Ivan <zhmur.ivan@gmail.com>
2024-01-27 22:32:41 +0200

    Write a succesful creation and connection to the database

--- Program.cs
+++ Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Dotnet_Exam.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+
+
+ConfigurationBuilder builder = new ConfigurationBuilder();
+
+builder.SetBasePath(Directory.GetCurrentDirectory());
+
+builder.AddJsonFile("Configuration/appsettings.json");
+
+IConfigurationRoot config = builder.Build();
+
+string? connectionString = config.GetConnectionString("DefaultConnection");
+
+DbContextOptionsBuilder<DotnetExambdContext> optionsBuilder = new DbContextOptionsBuilder<DotnetExambdContext>();
+
+DbContextOptions<DotnetExambdContext> options = optionsBuilder.UseMySql(connectionString, ServerVersion.Parse("8.0.30-mysql")).Options;
+
+using (DotnetExambdContext db = new DotnetExambdContext(options))
+{
+
+}
